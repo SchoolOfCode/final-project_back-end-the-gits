3205ce844813a45c4f9adddd08d62d910cf9a5ea
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-07-26 10:05:43 +0100

    server complete for 1st route path shoppinglist

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import logger from "morgan"
+import shoppingListRouter from "./routes/shoppinglist.js"
 
 const server = express();
 
@@ -8,11 +9,7 @@ server.use(cors())
 server.use(logger("dev"))
 server.use(express.json())
 
-server.use("/api/v1/shopping-list", (req, res) => {
-  res.json("Hello from the server")
-})
-
-
+server.use("/api/v1/shopping-list", shoppingListRouter)
 
 
 export default server
