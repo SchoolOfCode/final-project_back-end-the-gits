19e15d0f55399ea93ed0a0f330a37d4f700b8d2e
leeconroy <leeconroy1977@yahoo.co.uk>
2022-07-26 11:33:08 +0100

    schema updated to include shopping list name

--- models/shoppingSchema.js
+++ models/shoppingSchema.js
@@ -2,12 +2,18 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema
 
+//Schema to create object and specify type of data. Moongoose.schema is an inbuilt function.
+
 const shoppingSchema = new Schema({
   username: {
     type: String
   },
   item: {
     type: String
+  },
+  shoppingListName: {
+    type: String.fromCharCode,
+    required: true
   },
   completed: {
     type: Boolean
