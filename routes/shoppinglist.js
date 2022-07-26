ab78369e0f305ae415e3edb27b6e2249243f259e
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-07-26 11:41:35 +0100

    Imported controllers for the routes

--- routes/shoppinglist.js
+++ routes/shoppinglist.js
@@ -1,53 +1,28 @@
 import express from 'express';
-// TODO: IMPORT THE CONTROLLER
+import {
+  getShoppinglist,
+  getShoppinglistItem,
+  createShoppinglistItem,
+  updateShoppinglistItem,
+  deleteShoppinglistItem
+} from '../controllers/shoppinglistController.js'
 
 const router = express.Router();
 
 // Retrieves all the shopping lists
-router.get('/', (req, res) => {
-  res.json({
-    success: true,
-    payload: 'This returns all shopping lists'
-  });
-})
+router.get('/', getShoppinglist)
 
 // Retrieves all the items for a shopping by its id
-router.get('/:id', (req, res) => {
-  res.json({
-    success: true,
-    payload: 'This returns a shopping list'
-  });
-})
+router.get('/:id', getShoppinglistItem)
 
 // Adds a new item to the shopping list
-router.post('/:id', (req, res) => {
-  const { id } = req.params;
-  const { item } = req.body;
-  res.json({
-    success: true,
-    payload: `New item with body: ${item} added to list: ${id}`
-  });
-})
+router.post('/:id', createShoppinglistItem)
 
 // Amend list item
-router.patch('/:id/item/:item_id', (req, res) => {
-  const { id, item_id } = req.params;
-  const { created_by } = req.body;
-  res.json({
-    success: true,
-    payload: `New item with body: ${created_by} added to list: ${id} for item id: ${item_id}`
-  });
-})
+router.patch('/:id/item/:item_id', updateShoppinglistItem)
 
 // Amend list item
-router.delete('/:id/item/:item_id', (req, res) => {
-  const { id, item_id } = req.params;
-  const { created_by } = req.body;
-  res.json({
-    success: true,
-    payload: `New item with body: ${created_by} added to list: ${id} for item id: ${item_id}`
-  });
-})
+router.delete('/:id/item/:item_id', deleteShoppinglistItem)
 
 
 export default router;
