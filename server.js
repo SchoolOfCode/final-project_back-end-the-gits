a2abdceed31e6da2dad83976f7efcd71af22f76b
Carlos E Alford <10241920+CarlosEAM@users.noreply.github.com>
2022-08-08 14:49:34 +0100

    Comment out temp code

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ import express from "express"
 import cors from "cors"
 import logger from "morgan"
 import shoppingListRouter from "./routes/shoppinglist.js"
-import {expressjwt} from "express-jwt";
-import JwksRsa from "jwks-rsa";
+// import {expressjwt} from "express-jwt";
+// import JwksRsa from "jwks-rsa";
 
 
 const server = express();
 
-const checkJwt = expressjwt({
-    secret: JwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: `https://${process.env.AUTH0_API_DOMAIN}.us.auth0.com/.well-known/jwks.json`
-    }),
+// const checkJwt = expressjwt({
+//     secret: JwksRsa.expressJwtSecret({
+//       cache: true,
+//       rateLimit: true,
+//       jwksRequestsPerMinute: 5,
+//       jwksUri: `https://${process.env.AUTH0_API_DOMAIN}.us.auth0.com/.well-known/jwks.json`
+//     }),
   
-    audience: process.env.AUTH0_API_IDENTIFIER,
-    issuer: [`https://${process.env.AUTH0_API_DOMAIN}.us.auth0.com/`],
-    algorithms: ['RS256']
-  });
+//     audience: process.env.AUTH0_API_IDENTIFIER,
+//     issuer: [`https://${process.env.AUTH0_API_DOMAIN}.us.auth0.com/`],
+//     algorithms: ['RS256']
+//   });
 
 
-server.use(checkJwt)
+// server.use(checkJwt)
 
 server.use(cors())
 server.use(logger("dev"))
